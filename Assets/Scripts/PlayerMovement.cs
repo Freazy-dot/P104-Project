b6fd1e0a99e70abe043d6e638212d8654768791a
tikzyy <145325630+tikzyy@users.noreply.github.com>
2023-11-10 11:25:13 +0100

    argh

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,22 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody rb;
-
+    
+    // Player Movement Related
     [SerializeField] private float offset;
-
-    [SerializeField] private float speed;
+    [SerializeField] private float maxVel;
     [SerializeField] private float smoothTime;
-
     [SerializeField] private float minDistance;
-
     Vector2 currentVel;
 
+    // Boundaries
     [SerializeField] public float yBoundary;
     [SerializeField] public float xBoundary;
 
+    // User Specifications
+    private int screenSizeX = Screen.currentResolution.width;
+    private int screenSizeY = Screen.currentResolution.height;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -32,24 +35,23 @@ public class PlayerMovement : MonoBehaviour
 
 
         Quaternion rot = Quaternion.LookRotation(mouseWorldPosition - transform.position, Vector3.right) * Quaternion.Euler(offset, 0, 0);
-
-
+        
         //rotate player to mouse
         transform.rotation = rot;
-
-
-
+        
         //move code
 
         float distance = Vector3.Distance(transform.position, mouseWorldPosition);
             
-
+        // uhhh
+        
+        
         if (distance > minDistance)
         {
             Vector2 mouseWorldPosition2D = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
             mouseWorldPosition2D += ((Vector2)transform.position - mouseWorldPosition2D).normalized * minDistance;
-
-            transform.position = Vector2.SmoothDamp(transform.position, mouseWorldPosition2D, ref currentVel, 0.3f, speed);
+            
+            transform.position = Vector2.SmoothDamp(transform.position, mouseWorldPosition2D, ref currentVel, 0.3f, maxVel);
                 //// mouseWorldPosition2D var ikke brugt i transform.position.
                 //// Vi skal finde ud af hvordan vi får currentVel == speed (måske rename til maxVelocity)
                 //// KUN på de ydre kanter af skærmen. Dog skal vi også tage højde for forskellige aspekt ratios.
